3a20247d4d4a37c5425791fe79d896e499ebf6c0
parth <parth.logicalstreet@gmail.com>
2019-03-20 20:14:07 +0530

    Add Post model

---
+++ test/controllers/reports_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class ReportsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get reports_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get reports_index_url
+    assert_response :success
+  end
+
+end
