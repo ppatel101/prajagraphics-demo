9c86bfbc2726eb610b8a125eac62bfa9ae381607
parth <parth.logicalstreet@gmail.com>
2019-03-20 21:02:13 +0530

    Add Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'reports/new'
   get 'reports/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'reports#index'
+
 end
